e53605936173b3c4d16e04834e509205ae41009f
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-08-20 10:34:09 +0530

    second commit

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,4 +1,33 @@
 const Url = require('../models/Url.js');
 const generateShortCode = require('../utils/generateShortCode.js');
 
+const shortenUrl = async(req,res) => {
+    try {
+        const { originalUrl } = req.body;
+
+        if(!originalUrl){
+            return res.status(400).json({ error: `originalUrl is require` });
+        }
+
+
+        const shortCode = generateShortCode()
+
+        const newUrl = await Url.create({
+        originalUrl,
+        shortCode
+        })
+
+        res.status(201).json({
+        originalUrl: newUrl.originalUrl,
+        shortCode: newUrl.shortCode,
+        shortUrl:`http://localhost:3000/${newUrl.shortCode}`
+        });
+
+    } catch(error){
+        res.status(500).json({ error: `Something went wrong` })
+    }
+};
+
+module.exports = { shortenUrl }
+
 
