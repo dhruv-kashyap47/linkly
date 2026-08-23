caf32ed2637b6f7eeddb51484031c4edcdf78c91
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-08-23 16:54:50 +0530

    wonderful day it is

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { shortenUrl } = require("../controllers/urlController.js");
+const { shortenUrl, getUrlStats } = require("../controllers/urlController.js");
 
 router.post('/shorten', shortenUrl);
+router.get('/urls/:code/stats', getUrlStats);
 
 module.exports = router;
