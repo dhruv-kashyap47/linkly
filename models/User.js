de0b36514796ab0bdf6a17793464726ca03aeb79
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-08-30 17:22:57 +0530

    wonderful day it is

--- models/User.js
+++ models/User.js
@@ -17,3 +17,16 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+userSchema.pre('save', async function (next){
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema)
