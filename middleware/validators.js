a84840eb65d8e10460f06943d8a5c0f803decefd
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-08-30 09:42:26 +0530

    wonderful day it is

--- middleware/validators.js
+++ middleware/validators.js
@@ -3,6 +3,8 @@ const { body, validationResult } = require('express-validator');
 const validateShortenRequest = [
     body('originalUrl').trim().notEmpty().withMessage(`originalUrl is required`).isURL().withMessage(`originalUrl must be a valid URL`),
 
+    body('customAlias').optional().trim().isAlphanumeric().withMessage(`customAlias must contain only letters & numbers`).isLength({ min: 3, max: 20 }).withMessage(`customAlias must be between 3 & 20 characters`),
+
     (req, res, next) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()){
