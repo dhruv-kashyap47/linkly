62009cf5f2056215daa0fe62e94cbb8f794c3402
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-08-30 10:20:32 +0530

    wonderful day it is

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
