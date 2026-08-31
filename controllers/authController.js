57ca32285a39cd1b190b735d2a3559cddd91c542
dhruv-kashyap47 <dhruv_kashyap47@outlook.com>
2026-08-31 20:26:07 +0530

    wonderful day it is

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const AppError = require('../utils/AppError')
+
+const generateToken = (userId) => {
+    return jwt.sign( { id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+
+const register = async (req, res, next) => {
+    try {
+        
+    }
+}
+
+
+
